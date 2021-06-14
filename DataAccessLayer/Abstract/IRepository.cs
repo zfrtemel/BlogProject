838266b5cf6a2c51f09a0c5a2358dea53a4b1ae5
Ali <zaferaligunbey@gmail.com>
2021-06-14 10:30:30 +0300

    Katmanlar Hazır

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        List<T> List(Expression<Func<T, bool>> filter);
+        List<T> List();
+        int Insert(T p);
+        int Update(T p);
+        int Delete(T p);
+    }
+}
---
+++ EntityLayer/Concrete/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Admin
+    {
+         [Key]
+        public int AdminID { get; set; }
+        [StringLength(20)]
+        public String UserName { get; set; }
+        [StringLength(50)]
+        public string Password { get; set; }
+    }
+}
